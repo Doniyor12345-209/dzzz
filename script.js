420ee8f4c46c3afe61c552ca3b455c9944ac1d05
Doniyor12345-209 <ddonik502@gmail.com>
2025-11-10 22:44:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function Changecolor2() {
+    document.body.style.backgroundColor='#020100'
+    document.getElementById('h1').style.color ='white'
+    document.getElementById('li').style.color ='white'
+      document.getElementById('li2').style.color ='white'
+        document.getElementById('li3').style.color ='white'
+    document.getElementById('p').style.color ='white'
+    document.getElementById('btn3').style.color ='white'
+    document.getElementById('btn1').style.color ='white'
+}
+
+
+function Changecolor1() {
+    document.body.style.backgroundColor='#C4B5A5'
+    document.getElementById('h1').style.color ='#000000'
+    document.getElementById('li').style.color ='#000000'
+      document.getElementById('li2').style.color ='#000000' 
+        document.getElementById('li3').style.color ='#000000'
+    document.getElementById('p').style.color ='#000000'
+    document.getElementById('btn3').style.color ='#000000'
+    document.getElementById('btn1').style.color ='#000000'
+}
